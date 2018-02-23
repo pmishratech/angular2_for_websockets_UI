7f3ff3c34a41530f09ab543e51b72e6a054fb1a2
pankajmishra <pankajmishra@pankaj277601.Nagarro.local>
2018-02-23 22:06:18 +0530

    Angular 2 WebSockets

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+declare  var Stomp: any;
+declare var SockJS: any;
+declare var $: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private serverUrl = 'http://localhost:8888/socket'
+  public title = 'WebSockets chat';
+  private stompClient;
+
+  constructor(){
+    this.initializeWebSocketConnection();
+  }
+
+  initializeWebSocketConnection(){
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/kafka", (message) => {
+        if(message.body) {
+          $(".chat").append("<div class='message'>"+message.body+"</div>")
+          console.log(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(message){
+    this.stompClient.send("/app/send/message" , {}, message);
+    $('#input').val('');
+  }
+
+}
